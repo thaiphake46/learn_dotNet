21274e0e1b6599bf7f1b2f5212ddf8c4c9c95554
Huy Thai <huythaia123@gmail.com>
2022-02-24 22:25:44 +0700

    upload BaiTap2

---
+++ BaiTap2/BaiTap2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bai1:");
+            float a, b, c;
+            Console.Write("- Nhap a: ");
+            a = Convert.ToSingle(Console.ReadLine());
+            Console.Write("- Nhap b: ");
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.Write("- Nhap c: ");
+            c = Convert.ToSingle(Console.ReadLine());
+            float max = a;
+            max = max > b ? max : b;
+            max = max > c ? max : c;
+            Console.WriteLine("\t+ Gia tri lon nhat: " + max);
+
+            Console.WriteLine("Bai2:");
+            const float PI = 3.1416f;
+            Console.Write("- Nhap ban kinh r: ");
+            float r;
+            r = Convert.ToSingle(Console.ReadLine());
+            float chuVi = r * 2 * PI;
+            float dienTich = r * r * PI;
+            Console.WriteLine("\t+ Chu vi hinh tron: " + chuVi);
+            Console.WriteLine("\t+ Dien tich hinh tron: " + dienTich);
+
+            Console.WriteLine("Bai3:");
+            Console.WriteLine("\t[ Nhap thong tin the sinh vien ]");
+            Console.Write("- Nhap ho ten:    ");
+            string name = Console.ReadLine();
+            Console.Write("- Nhap ngay sinh: ");
+            string ngaySinh = Console.ReadLine();
+            Console.Write("- Nhap nganh hoc: ");
+            string nganhHoc = Console.ReadLine();
+            Console.Write("- Nhap nam hoc:   ");
+            string namHoc = Console.ReadLine();
+            Console.Write("- Nhap MSSV:      ");
+            string mssv = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("************ THE SINH VIEN ************");
+            Console.WriteLine("*\t+ Ho ten:    " + name);
+            Console.WriteLine("*\t+ Ngay sinh: " + ngaySinh);
+            Console.WriteLine("*\t+ Nganh hoc: " + nganhHoc);
+            Console.WriteLine("*\t+ Nam hoc:   " + namHoc);
+            Console.WriteLine("*\t+ MSSV:      " + mssv);
+            Console.WriteLine("***************************************");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
